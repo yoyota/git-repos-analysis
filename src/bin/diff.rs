48c8530b93ebda5329c066e3ab9ec53a97dc674e
YongTak Yoo <yongtaky@gmail.com>
2025-02-14 14:27:11 +0900

    Write git diff to txt file

--- src/bin/diff.rs
+++ src/bin/diff.rs
@@ -1,44 +1,81 @@
-use git2::{DiffOptions, DiffStatsFormat, ErrorCode, Oid, Repository};
+use git2::{Commit, DiffOptions, DiffStats, DiffStatsFormat, Repository};
 use regex::Regex;
-use std::fs::File;
-use std::io::{self, BufRead};
+use std::fs::{File, OpenOptions};
+use std::io::{self, BufRead, BufWriter, Write};
 use std::str::from_utf8;
 
 fn main() {
     let file_path = "/home/yoyota/hobby/gitlab-clone-to-local/clone_path.txt";
     let file = File::open(file_path).unwrap();
     let reader = io::BufReader::new(file);
+    let write_file_path = "/home/yoyota/hobby/gitlab-clone-to-local/test.txt";
 
-    for line in reader.lines().take(1).flatten() {
+    let open_opotions = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .open(write_file_path)
+        .unwrap();
+    let mut writer = BufWriter::new(open_opotions);
+
+    for line in reader.lines().flatten() {
         let repo = Repository::open(line).unwrap();
         let mut revwalk = repo.revwalk().unwrap();
         revwalk.push_glob("refs/*").unwrap();
-        for oid in revwalk.skip(1).take(1) {
-            print_diff(&repo, oid.unwrap());
+
+        for commit in revwalk
+            .filter_map(|r| r.ok())
+            .filter_map(|oid| repo.find_commit(oid).ok())
+            .filter(|commit| {
+                commit
+                    .author()
+                    .name()
+                    .map_or(false, |name| name == "yoyota" || name == "YongTak Yoo")
+            })
+        {
+            let lines = get_diff_lines(&repo, commit);
+            lines.iter().for_each(|line| {
+                writeln!(writer, "{}", line).unwrap();
+            });
         }
     }
 }
 
-fn print_diff(repo: &Repository, oid: Oid) {
-    let commit = repo.find_commit(oid).unwrap();
-    println!("{}", commit.id());
-
+fn get_diff_lines(repo: &Repository, commit: Commit) -> Vec<String> {
+    println!("{}", commit.message().unwrap_or(""));
     let old_tree = commit.parents().next().map(|p| p.tree().unwrap());
     let tree = commit.tree().unwrap();
 
     let mut diff_options = DiffOptions::new();
+    let diff = repo
+        .diff_tree_to_tree(old_tree.as_ref(), Some(&tree), Some(&mut diff_options))
+        .unwrap();
+    let stats = diff.stats().unwrap();
+    filter_out_large_chang_files(stats)
+        .iter()
+        .for_each(|file_name| {
+            diff_options.pathspec(&file_name);
+        });
     diff_options
+        .pathspec(" ")
         .context_lines(5)
         .ignore_blank_lines(true)
         .ignore_whitespace(true)
         .ignore_whitespace_change(true)
         .ignore_whitespace_eol(true);
 
-    let diff = repo
-        .diff_tree_to_tree(old_tree.as_ref(), Some(&tree), Some(&mut diff_options))
-        .unwrap();
-    let stats = diff.stats().unwrap();
+    let mut diff_lines = Vec::new();
 
+    let _ = diff.print(git2::DiffFormat::Patch, |_, _, line| {
+        if let Ok(text) = from_utf8(line.content()) {
+            diff_lines.push(format!("{}{}", line.origin(), text));
+        }
+        true
+    });
+
+    diff_lines
+}
+
+fn filter_out_large_chang_files(stats: DiffStats) -> Vec<String> {
     let s = stats
         .to_buf(DiffStatsFormat::FULL, 100)
         .unwrap()
@@ -57,42 +94,5 @@ fn print_diff(repo: &Repository, oid: Oid) {
                 .parse::<u32>()
                 .map_or_else(|_| None, |cs| (cs < 1000).then(|| file_name))
         })
-        .for_each(|file_name| {
-            diff_options.pathspec(&file_name);
-        });
-
-    diff_options.pathspec(" ");
-    let diff = repo
-        .diff_tree_to_tree(old_tree.as_ref(), Some(&tree), Some(&mut diff_options))
-        .unwrap();
-    if let Err(e) = diff.print(git2::DiffFormat::Patch, |_, _, line| {
-        let text = from_utf8(line.content()).unwrap();
-        print!("{}{}", line.origin(), text);
-        true
-    }) {
-        if e.code() == ErrorCode::User {
-            return;
-        }
-        panic!("Error printing diff: {}", e);
-    }
+        .collect()
 }
-
-//         // Print the diff
-//     }
-//  else {
-//     // If there's no parent, show the diff for the initial commit
-//     let mut diff_options = DiffOptions::new();
-//     let diff = repo
-//         .diff_tree_to_tree(None, Some(&tree), Some(&mut diff_options))
-//         .unwrap();
-
-//     diff.print(git2::DiffFormat::Patch, |delta, _, line| {
-//         println!(
-//             "{}: {:?}",
-//             delta.new_file().path().unwrap().to_string_lossy(),
-//             line.content()
-//         );
-//         true
-//     })
-//     .unwrap();
-// }
